c96495b21459317e9bfe8c1ad781ed9e2627dd8a
shawn <liushawn@outlook.com>
2018-03-14 16:26:03 +0800

    01 两数相加

---
+++ src/leetcode/AddTwoNumbers.java
@@ -0,0 +1,39 @@
+//package leetcode;
+//
+//public class AddTwoNumbers {
+//	
+//	/**
+//	 * Definition for singly-linked list.
+//	 * public class ListNode {
+//	 *     int val;
+//	 *     ListNode next;
+//	 *     ListNode(int x) { val = x; }
+//	 * }
+//	 */
+//	
+//    public static int []  addTwoNumbers(int [] l1, int [] l2) {
+//    	int [] a = new int [3];
+//    	
+//    	for(int i=l1.length-1;i>=0;i--){
+//    		a[i] = l1[i] + l2[i];
+//    	}
+//    	
+//    	for()
+//    	
+//    	return a;
+//    }
+//    
+//    public static void main(String[] args) {
+//		int [] l1 = {2, 4, 3};
+//		int [] l2 = {5, 6, 4};
+//		int [] array = new int [l1.length];
+//		System.arraycopy(addTwoNumbers(l1,l2), 0, array, 0, l1.length);
+//		//打印数组
+//		for(int each:array){
+//			System.out.println(each);
+//		}
+//	}
+//    
+//
+//}
+//
