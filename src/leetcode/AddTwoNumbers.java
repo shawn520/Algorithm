ce05f81d3a4e1699d1732b229a4bfa8b3585931a
shawn <liushawn@outlook.com>
2018-03-24 18:36:02 +0800

    二分查找

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -1,39 +1,36 @@
-//package leetcode;
-//
-//public class AddTwoNumbers {
-//	
-//	/**
-//	 * Definition for singly-linked list.
-//	 * public class ListNode {
-//	 *     int val;
-//	 *     ListNode next;
-//	 *     ListNode(int x) { val = x; }
-//	 * }
-//	 */
-//	
-//    public static int []  addTwoNumbers(int [] l1, int [] l2) {
-//    	int [] a = new int [3];
-//    	
-//    	for(int i=l1.length-1;i>=0;i--){
-//    		a[i] = l1[i] + l2[i];
-//    	}
-//    	
-//    	for()
-//    	
-//    	return a;
-//    }
-//    
-//    public static void main(String[] args) {
-//		int [] l1 = {2, 4, 3};
-//		int [] l2 = {5, 6, 4};
-//		int [] array = new int [l1.length];
-//		System.arraycopy(addTwoNumbers(l1,l2), 0, array, 0, l1.length);
-//		//打印数组
-//		for(int each:array){
-//			System.out.println(each);
-//		}
-//	}
-//    
-//
-//}
-//
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+class Solution {
+   public  static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+    while(p.next != null || q.next != null){
+    	int x = p.next.val;
+    	int y = q.next.val;
+    	int sum = carry + x + y;
+    	carry = sum / 10;
+    	curr.next = new ListNode(sum % 10);
+    	curr = curr.next;
+    	if(p!=null)
+    		p = p.next;
+    	if(q!=null)
+    		q = q.next;
+    } 
+    if(carry>0)
+    	curr.next =new ListNode(carry);
+        return dummyHead.next;
+  }  
+}
