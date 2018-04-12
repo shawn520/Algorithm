f8534689bef3fe88e77ad790f7de28369da960ba
shawn <liushawn@outlook.com>
2018-04-12 20:34:23 +0800

    二分查找的递归实现

---
+++ src/search/Bisearch6.java
@@ -0,0 +1,37 @@
+package search;
+
+import java.util.Scanner;
+
+public class Bisearch6 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int key = in.nextInt();
+		
+		int [] a = {4,6,8,10,14};
+		
+		int ans = search(a,key);
+		System.out.println(ans);
+	}
+
+	private static int search(int[] a, int key) {
+		// TODO Auto-generated method stub
+		
+		return bisearch(a,key,0,a.length-1);
+	}
+
+	private static int bisearch(int[] a, int key, int left, int right) {
+		// TODO Auto-generated method stub
+		while(left<right){
+			int mid = left+ (right-left)/2;
+			if(key == a[mid]){
+				return mid+1;
+			}else if(key <a[mid]){
+				right = mid - 1;
+			}else{
+				left = mid + 1;
+			}
+		}
+		return -1;
+	}
+}
