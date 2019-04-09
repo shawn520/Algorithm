3950d8649c734279a82127c0aec584b2e94d20e2
柳潇 <liuxiao_sx@qiyi.com>
2019-04-09 09:02:37 +0800

    斐波那契数列

---
+++ src/offer/group1/test7/Solution.java
@@ -0,0 +1,23 @@
+package offer.group1.test7;
+
+/**
+ * 描述:
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，
+ * 请你输出斐波那契数列的第n项（从0开始，第0项为0）。n<=39
+ */
+
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(n==2) {
+            return 1;
+        }
+        return Fibonacci(n-2) + Fibonacci(n-1);
+
+    }
+}
