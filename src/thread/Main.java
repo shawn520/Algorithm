acba0567689e6acf70375a804731a28b7a918d72
柳潇 <liuxiao_sx@qiyi.com>
2019-04-09 09:06:40 +0800

    多线程

---
+++ src/thread/Main.java
@@ -0,0 +1,13 @@
+package thread;
+
+public class Main {
+    public static void main(String [] args) {
+        /*MyThread myThread = new MyThread();
+        myThread.start();*/
+        Thread thread = new Thread(new MyRunnable());
+        for(int i=0;i<100;i++) {
+            System.out.println("MainThread:" + i);
+
+        }
+    }
+}
