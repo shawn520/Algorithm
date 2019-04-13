8f0581a7a67c2dce8e893d2ff6851925187d0fca
柳潇 <liuxiao_sx@qiyi.com>
2019-04-13 09:43:30 +0800

    实现基于CAS线程安全的计数器方法safeCount()和非线程安全计数器count

--- src/others/concurent/Counter.java
+++ src/others/concurent/Counter.java
@@ -59,6 +59,7 @@ public class Counter {
      * 使用CAS实现线程安全计数
      */
     private void safeCount() {
+        //自旋CAS,直到成功为止
         for(;;) {
             int i = atomicI.get();
             boolean suc = atomicI.compareAndSet(i, ++i);
