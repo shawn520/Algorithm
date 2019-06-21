d6f6984351c1c580ea95d2e926d94aeae4591748
shawn <liushawn@outlook.com>
2019-06-21 23:05:53 +0800

    给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。

---
+++ src/offer/group2/test12/Main.java
@@ -0,0 +1,8 @@
+package offer.group2.test12;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.test();
+    }
+}
--- src/offer/group2/test12/Solution.java
+++ src/offer/group2/test12/Solution.java
@@ -1,20 +1,36 @@
 package offer.group2.test12;
 
 
+import org.junit.Test;
+
 /**
  * 题目描述
  * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ * @author Shawn
  */
 public class Solution {
-
+    @Test
     public void test() {
-        System.out.println(Power(8.0, 3));
+        System.out.println(Power(2, -3));
 
     }
 
     public double Power(double base, int exponent) {
+        boolean sign = true;
+        if(exponent < 0) {
+            sign = false;
+        }
+        double pos_exponent = Math.sqrt( exponent * exponent);
+        double result = 1;
+        while (pos_exponent > 0) {
+            result = result * base;
+            pos_exponent --;
 
+        }
 
-        return Math.sqrt(base);
+        if(!sign) {
+            result = 1 / result;
+        }
+        return result;
     }
 }
