583067533b4f089de6dd2cf6341f6f4887c4471b
shawn <liushawn@outlook.com>
2019-06-28 20:41:11 +0800

    操作给定的二叉树，将其变换为源二叉树的镜像。

--- src/offer/group2/test18/Solution.java
+++ src/offer/group2/test18/Solution.java
@@ -1,8 +1,5 @@
 package offer.group2.test18;
 
-
-
-
 class TreeNode {
     int val = 0;
     TreeNode left = null;
@@ -14,7 +11,6 @@ class TreeNode {
     }
 
 }
-
 
 /**
  * 操作给定的二叉树，将其变换为源二叉树的镜像。
