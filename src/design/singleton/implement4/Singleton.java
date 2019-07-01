1570e25319ea8442c619caae77b23d2e5a345ae6
shawn <liushawn@outlook.com>
2019-07-01 10:42:43 +0800

    单例模式的双重校验锁实现。

--- src/design/singleton/implement4/Singleton.java
+++ src/design/singleton/implement4/Singleton.java
@@ -1,11 +1,9 @@
 package design.singleton.implement4;
 
-import others.concurent.chapter4.Synchronized;
-
 /**
- * 单例模式的懒汉式线程安全实现
- * 优点：需要时加载
- * 缺点：加锁效率低
+ * 单例模式的双重校验锁实现
+ * 优点：需要时加载，线程安全，效率高
+ * 缺点：实现稍复杂
  * @author Shawn
  * @date 2019/7/1
  */
