412ec4978967b99806cc784a632f27188db13b68
shawn <liushawn@outlook.com>
2019-07-01 10:51:32 +0800

    单例模式的枚举实现

---
+++ src/design/singleton/implement6/SingletonDemo.java
@@ -0,0 +1,13 @@
+package design.singleton.implement6;
+
+/**
+ * @author Shawn
+ * @date 2019/7/1
+ */
+public class SingletonDemo {
+
+    public static void main(String[] args) {
+
+        Singleton.INSTANCE.showMsg();
+    }
+}
