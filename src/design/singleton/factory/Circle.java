2fb4c8e1e6d1705ebebb9b53b895fdbadd2782d4
shawn <liushawn@outlook.com>
2019-07-02 21:22:08 +0800

    工厂模式的Demo实现

---
+++ src/design/singleton/factory/Circle.java
@@ -0,0 +1,12 @@
+package design.singleton.factory;
+
+/**
+ * @author Shawn
+ * @date 2019/7/2
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("draw Circle");
+    }
+}
---
+++ src/design/singleton/factory/Shape.java
@@ -0,0 +1,10 @@
+package design.singleton.factory;
+
+/**
+ * @author Shawn
+ * @date 2019/7/2
+ */
+public interface Shape {
+    void draw();
+
+}
