06206db0fe960648084ee3cca3fd95973289036b
shawn <liushawn@outlook.com>
2019-07-19 10:25:53 +0800

    输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字

--- src/offer/group2/test19/Solution.java
+++ src/offer/group2/test19/Solution.java
@@ -3,6 +3,8 @@ package offer.group2.test19;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
 
 /**
  * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
@@ -28,7 +30,6 @@ public class Solution {
 //        int [][] matrix = {{1},{2},{3},{4},{5}};
         // 边界测试：只有一行一列
         int [][] matrix = {{1,2,3,4}, {5,6,7,8}};
-
 
         ArrayList<Integer> list = printMatrix(matrix);
 
