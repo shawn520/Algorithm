2f6d37b220ec47df2d196e317d6a1259b81532d5
shawn <liushawn@outlook.com>
2019-07-20 16:51:23 +0800

    整数中1出现的次数（从1到n整数中1出现的次数）

--- src/offer/group4/test31/Solution.java
+++ src/offer/group4/test31/Solution.java
@@ -1,4 +1,4 @@
-package offer;
+package offer.group4.test31;
 
 import org.junit.Test;
 
---
+++ src/offer/group4/test32/Solution.java
@@ -0,0 +1,44 @@
+package offer.group4.test32;
+
+import org.junit.Test;
+
+/**
+ * 整数中1出现的次数（从1到n整数中1出现的次数）
+ * @author Shawn
+ * @date 2019/7/20
+ */
+public class Solution {
+
+    @Test
+    public void test() {
+        // 功能测试
+//        int n = 13;
+
+        // 边界测试
+//        int n = 1;
+
+        // 负值测试
+        int n = -1;
+
+        int result = NumberOf1Between1AndN_Solution(n);
+        System.out.println(result);
+
+    }
+
+    public int NumberOf1Between1AndN_Solution(int n) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=1; i<=n; i++) {
+            builder.append(i);
+        }
+
+        char [] chars = builder.toString().toCharArray();
+        int count = 0;
+        for(int i=0; i< chars.length; i++) {
+            if(chars[i] == '1') {
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
