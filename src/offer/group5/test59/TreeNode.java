9bf7d1fbe58eda928b0bb894ab26dbcb1a02462a
shawn <liushawn@outlook.com>
2019-08-06 21:45:42 +0800

    对称的二叉树

---
+++ src/offer/group5/test59/TreeNode.java
@@ -0,0 +1,17 @@
+package offer.group5.test59;
+
+/**
+ * @author Shawn
+ * @date 2019/8/6
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
