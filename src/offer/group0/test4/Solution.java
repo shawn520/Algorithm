965d05a833d7948f4ff59c729ec9d0ae3d04d671
shawn <liushawn@outlook.com>
2019-08-12 11:35:25 +0800

    重建二叉树

--- src/offer/group0/test4/Solution.java
+++ src/offer/group0/test4/Solution.java
@@ -2,9 +2,19 @@ package offer.group0.test4;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 /**
+ * 重建二叉树
+ * 题目描述
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
+ * 则重建二叉树并返回。
+ *
+ * 分析：
+ * 根据例子，按照前序遍历和中序遍历构造二叉树的规则写代码
+ *
  * @author Shawn
  * @date 2019/8/5
  */
@@ -22,37 +32,39 @@ public class Solution {
     }
 
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
-
-        if(null == pre || null == in || pre.length != in.length) {
+        if(null == pre || pre.length <= 0) {
             return null;
         }
-        int val = pre[0];
-        TreeNode node = new TreeNode(val);
-        if(pre.length == 1) {
-            return node;
-        }
-        int mid = Arrays.binarySearch(in,val);
-        if(mid > 0 ) {
-            int [] pre1 = new int[mid];
-            int [] in1 = new int[mid];
-            for(int i = 0; i < mid; i++) {
-                pre1[i] = pre[i+1];
-                in1[i] = in[i];
-            }
-            node.left = reConstructBinaryTree(pre1, in1);
+        int length = in.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int j = 0; j < length; j++) {
+            map.put(in[j], j);
         }
 
-        if(mid >= 0 && mid < in.length -1) {
-            int length = in.length - mid - 1;
-            int [] pre2 = new int [length];
-            int [] in2 = new int [length];
-            for(int i = 0; i < length; i++) {
-                pre2[i] = pre[i+mid+1];
-                in2[i] = in[i+mid+1];
-            }
-            node.right = reConstructBinaryTree(pre2, in2);
-        }
+        // 重构二叉树
+        TreeNode node = rebuild(pre, in, 0, length-1, 0, length-1, map);
+
         return node;
+
+
+    }
+
+    private TreeNode rebuild(int[] pre, int[] in, int i, int j, int m, int n,
+                             HashMap<Integer, Integer> map) {
+        if(i <= j) {
+            int val = pre[i];
+            TreeNode node = new TreeNode(val);
+            int index = map.get(val);
+            int length = index - m;
+            TreeNode left = rebuild(pre, in, i+1, i+length, m, m+length-1, map);
+            TreeNode right = rebuild(pre, in, i+length+1, j, index+1, n, map);
+            node.left = left;
+            node.right = right;
+            return node;
+        }
+
+        return null;
     }
 
 
