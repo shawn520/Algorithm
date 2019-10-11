6ee3bafdb4dc24637710187fca40a3c73949c91c
shawn <liushawn@outlook.com>
2019-10-11 19:21:03 +0800

    一个基于线程池技术的简单web服务器

---
+++ src/others/concurent/chapter4/pool/thread/ThreadPool.java
@@ -0,0 +1,18 @@
+package others.concurent.chapter4.pool.thread;
+
+/**
+ * @author Shawn
+ * @date 2019/10/10
+ */
+public interface ThreadPool<Job extends Runnable> {
+
+    void execute(Job job);
+
+    void shutdown();
+
+    void addWorkers(int num);
+
+    void removeWorkers(int num);
+
+    int getJobSize();
+}
