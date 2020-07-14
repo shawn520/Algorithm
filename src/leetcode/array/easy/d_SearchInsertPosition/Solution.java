dba53d8869a48ab10f435d6397cdd8877f8d50b6
shawn <liushawn@outlook.com>
2020-07-14 22:09:17 +0800

    35. Search Insert Position

---
+++ src/leetcode/array/easy/d_SearchInsertPosition/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.array.easy.d_SearchInsertPosition;
+
+/**
+ * 思路：
+ * 二分查找
+ * 注意：判断条件是小于等于
+ * @author Shawn
+ * @date 2020/7/14
+ */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return searchInsert(nums, target, 0, nums.length-1);
+    }
+    public int searchInsert(int[] nums, int target, int left, int right) {
+        if(left<=right) {
+            int mid = (left + right)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) return searchInsert(nums, target, left, mid-1);
+            else return searchInsert(nums, target, mid+1, right);
+        }
+        return left;
+    }
+}
