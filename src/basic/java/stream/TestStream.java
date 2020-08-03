763981ca9f9cd5d006a10ad222a32768b036c368
shawn <liushawn@outlook.com>
2020-08-03 23:08:02 +0800

    stream常用方法复习

---
+++ src/basic/java/stream/TestStream.java
@@ -0,0 +1,24 @@
+package basic.java.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Shawn
+ * @date 2020/8/3
+ */
+public class TestStream {
+
+  @Test
+  public void test() {
+    List<String> strings = Arrays.asList("Hollis", "HollisChuang", "hollis", "Hello", "HelloWorld", "Hollis");
+    strings.stream().filter(p -> p.length()<7)
+            .map(p -> p.length())
+            .sorted()
+            .limit(3)
+            .distinct()
+            .forEach(System.out :: println);
+  }
+}
