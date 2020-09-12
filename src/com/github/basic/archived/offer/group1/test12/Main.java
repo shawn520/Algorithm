6fa599f1acbb951a3cb8af376f24dd671df45ba5
shawn <liushawn@outlook.com>
2020-09-12 09:09:13 +0800

    delete all

--- src/com/github/basic/archived/offer/group1/test12/Main.java
+++
@@ -1,8 +0,0 @@
-package com.github.basic.archived.offer.group1.test12;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.test();
-    }
-}
--- src/com/github/basic/archived/offer/online/netease/test3/Main.java
+++
@@ -1,8 +0,0 @@
-package com.github.basic.archived.offer.online.netease.test3;
-
-/**
- * @author Shawn
- * @date 2019/8/3
- */
-public class Main {
-}
