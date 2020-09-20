183e6b167d6166b6d6800036953e55a1eef63688
shawn <liushawn@outlook.com>
2020-09-20 12:02:37 +0800

    BIO demo

--- src/main/java/com/github/demo/basic/java/io/bio/BIOServer.java
+++ src/main/java/com/github/demo/basic/java/io/bio/BIOServer.java
@@ -1,59 +1,90 @@
 package com.github.demo.basic.java.io.bio;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
+ * BIO
+ * main线程为每一个连接创建一个线程，没有新的连接时,main线程阻塞在在accept()方法上。
+ * 当前连接没有数据传输时，线程阻塞在read()方法
+ *
+ * cmd客户端telnet 127.0.0.1 6666
+ *  'CTRL+]' 开始发送消息 send hello100
+ *
  * @author Shawn
- * @date 2020/7/20
+ * @date 2020/9/7
  */
 public class BIOServer {
-    public void initBIOServer(int port)
-    {
-        ServerSocket serverSocket = null;//服务端Socket
-        Socket socket = null;//客户端socket
-        BufferedReader reader = null;
-        String inputContent;
-        int count = 0;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println(stringNowTime() + ": serverSocket started");
-            while(true)
-            {
-                socket = serverSocket.accept();
-                System.out.println(stringNowTime() + ": id为" + socket.hashCode()+ "的Clientsocket connected");
-                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                while ((inputContent = reader.readLine()) != null) {
-                    System.out.println("收到id为" + socket.hashCode() + "  "+inputContent);
-                    count++;
-                }
-                System.out.println("id为" + socket.hashCode()+ "的Clientsocket "+stringNowTime()+"读取结束");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                reader.close();
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+  public static void main(String[] args) throws IOException {
+    //线程池机制
+
+    //思路：
+    // 1. 创建一个线程池
+
+    ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
+
+    // 创建ServerSocket
+    ServerSocket serverSocket = new ServerSocket(6666);
+
+    System.out.println("服务器启动了");
+
+    while (true) {
+      //监听，等待客户端连接
+      System.out.println("等待连接....");
+      final Socket socket = serverSocket.accept();
+      System.out.println("连接到一个客户端");
+
+      //2. 如果有客户端连接，就创建一个线程与之通讯（单独写一个方法）
+      newCachedThreadPool.execute(new Runnable() {
+        @Override
+        public void run() { //重写
+          //可以和客户端通讯
+          handler(socket);
         }
-    }
-    public String stringNowTime()
-    {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return format.format(new Date());
-    }
+      });
 
-    public static void main(String[] args) {
-        BIOServer server = new BIOServer();
-        server.initBIOServer(8888);
 
     }
+
+
+  }
+
+  public static void handler(Socket socket) {
+    System.out.println("线程id = " + Thread.currentThread().getId() + " 名字" + Thread.currentThread().getName());
+
+    byte[] bytes = new byte[1024];
+    // 通过socket获取输入流
+    try {
+      InputStream inputStream = socket.getInputStream();
+
+      //循环读取客户端发送的数据
+      while (true) {
+        System.out.println("线程id = " + Thread.currentThread().getId() + " 名字" + Thread.currentThread().getName());
+
+        System.out.println("read....");
+        int read = inputStream.read(bytes);
+        if(read != -1) {
+          System.out.println(new String(bytes, 0 , read)); //输出客户端发送的数据
+        } else {
+          break;
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      System.out.println("关闭和client的连接");
+      try {
+        socket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+
+  }
 }
