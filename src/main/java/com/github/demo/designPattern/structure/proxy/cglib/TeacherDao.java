283ed1c37db5a21e67183804b5934de9fa1925bb
shawn <liushawn@outlook.com>
2020-10-13 22:45:49 +0800

    cglib动态代理

---
+++ src/main/java/com/github/demo/designPattern/structure/proxy/cglib/TeacherDao.java
@@ -0,0 +1,12 @@
+package com.github.demo.designPattern.structure.proxy.cglib;
+
+/**
+ * @author Shawn
+ * @date 2020/10/13
+ */
+public class TeacherDao {
+
+    public void teach() {
+        System.out.println("teaching....");
+    }
+}
