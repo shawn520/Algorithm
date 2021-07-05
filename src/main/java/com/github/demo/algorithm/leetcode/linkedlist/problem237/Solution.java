eeecdfc99104d5b977549e58a3767b808a0cbddd
shawn <liushawn@outlook.com>
2021-07-05 21:29:50 +0800

    237. 删除链表中的节点

--- src/main/java/com/github/demo/algorithm/leetcode/linkedlist/problem237/Solution.java
+++ src/main/java/com/github/demo/algorithm/leetcode/linkedlist/problem237/Solution.java
@@ -10,10 +10,6 @@ class Solution {
         node.val = node.next.val;
         node.next = node.next.next;
     }
-
-    public static void main(String[] args) {
-        System.out.println(1<<1);
-    }
 }
 
 
